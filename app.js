1de66ecb2b5ce115bc7287147826df5b501a7a9f
Ketan <ketankatore3367@gmail.com>
2020-01-22 12:13:39 +0530

    Static Files Middleware Added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("Hey ! This is demo application")
 })
+app.use(express.static('images'));
 
 console.log("Setting up routes.")
 require("./routes")(app)
