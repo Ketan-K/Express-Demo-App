b8dff940e44a9fa0ca460d3bf1d0092d6bd792c1
Ketan <ketankatore3367@gmail.com>
2020-01-23 17:06:50 +0530

    Public Folder added for static files

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 "use strict"
 const express = require("express")
 const config = require("./config/config")
-const app = express()
+const app = express(),
+  path = require("path")
 app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hey ! This is demo application")
 })
-app.use(express.static('images'));
+
+
+app.use(function (req, res, next) {
+  var filename = path.basename(req.url);
+  var extension = path.extname(filename);
+  if (extension === '.xlsx')
+    console.log("The file " + filename + " was requested.");
+  next();
+},
+  express.static('public'));
 
 console.log("Setting up routes.")
 require("./routes")(app)
