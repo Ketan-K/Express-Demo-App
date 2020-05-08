a4a8a3b6ec6495aab42f4c5dbbb7cd8662caac1c
Ketan-K <42907652+Ketan-K@users.noreply.github.com>
2020-05-08 11:30:45 +0530

    Added cors

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const app = express(),
 
 app.use(express.json())
 app.use(cors())
-
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,API-KEY");
+    next();
+});
 app.get("/", (req, res) => {
   res.send("Hey ! This is demo application")
 })
